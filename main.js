13026093c703ea910eb9116559f72b60d3047db2
mikelyferreiradossantos <147520312+mikelyferreiradossantos@users.noreply.github.com>
2025-04-02 16:45:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo")
 
 for (let i=0; i < botoes.length; i++){
 
@@ -6,9 +7,17 @@ for (let i=0; i < botoes.length; i++){
 
         for (let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            textos[j]. classList.remove("ativo")
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     };
-
 };
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2029-10-05T00:00:00");
+
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
